cc6585c697006e6b882e6a0f7209c1421c9a1ae9
Ryan S. Northrup <northrup@yellowapple.us>
2015-11-23 09:57:25 -0800

    Slightly-more-accurate `fucking_coffee.rb`
    Implemented a `delay` variable, and set it to 24 instead of 64 to better reflect the original story.

--- fucking_coffee.rb
+++ fucking_coffee.rb
@@ -11,10 +11,11 @@ require 'net/telnet'
 coffee_machine_ip = '10.10.42.42'
 password = '1234'
 password_prompt = 'Password: '
+delay = 24
 
 con = Net::Telnet.new('Host' => coffee_machine_ip)
 con.cmd('String' => password, 'Match' => /#{password_prompt}/)
 con.cmd('sys brew')
-sleep 64
+sleep delay
 con.cmd('sys pour')
 con.close
