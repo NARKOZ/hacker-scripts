9066736d51f15dbb42da3c1435e2ed2ffcc4b832
Cheng Huang <chuang@mdsol.com>
2015-11-25 17:19:07 -0500

    fix a typo

--- smack_my_bitch_up.ps1
+++ smack_my_bitch_up.ps1
@@ -7,7 +7,7 @@ if ($DAYOFWEEK -eq 6 -or $DAYOFWEEK -eq 7) {
 
 # Exit early if no sessions with my username are found
 if ((QWINSTA $env:USERNAME | measure).Count -gt 0){
-    #return
+    return
 }
 
 # Phone numbers
