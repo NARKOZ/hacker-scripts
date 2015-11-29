dc7188cf432b79c0f4dcd4f9aea5a6425a63ad5a
alexesprit <alex.esprit@gmail.com>
2015-11-30 00:08:19 +0300

    Fix sms sending
    When post data is set as text string, some characters like `plus` were not escaped.

--- powershell/smack_my_bitch_up.ps1
+++ powershell/smack_my_bitch_up.ps1
@@ -14,9 +14,6 @@ if (-not (QWINSTA | FINDSTR $env:USERNAME)) {
 $MY_NUMBER='+xxx'
 $HER_NUMBER='+xxx'
 
-$TWILIO_ACCOUNT_SID = 'xxx'
-$TWILIO_AUTH_TOKEN = 'xxx'
-
 $REASONS=
   'Working hard',
   'Gotta ship this feature',
@@ -26,14 +23,18 @@ $RAND = Get-Random -Maximum $REASONS.Count
 
 $MSG="Late at work. $REASONS[$RAND]"
 
+$API_URL = "https://api.twilio.com/2010-04-01/Accounts/$env:TWILIO_ACCOUNT_SID/Messages"
+$BASE64AUTHINFO = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $env:TWILIO_ACCOUNT_SID,$env:TWILIO_AUTH_TOKEN)))
+$body = @{
+    From = $MY_NUMBER;
+    To = $HER_NUMBER;
+    Body = $MSG;
+}
 
-$BASE64AUTHINFO = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $TWILIO_ACCOUNT_SID,$TWILIO_AUTH_TOKEN)))
-
-#Send a text messag and Log errors
+#Send a text message and Log errors
 try{
-    Invoke-RestMethod -Method Post -Headers @{Authorization=("Basic {0}" -f $BASE64AUTHINFO)} "https://api.twilio.com/2010-04-01/Accounts/$TWILIO_ACCOUNT_SID/Messages" -Body "From=$MY_NUMBER&To=$HER_NUMBER&Body=$MSG" > $null
+    Invoke-RestMethod -Method Post -Headers @{Authorization=("Basic {0}" -f $BASE64AUTHINFO)} $API_URL -Body $body > $null
 }
 catch{
     Write-Host "Failed to send SMS: $_"
 }
-
