7f9d6b1c4d076380aa2665d6b0db9d69c2da6ae9
Chris Topaloudis <topless@gmail.com>
2015-12-04 15:48:45 +0100

    Indentation fix

--- python/smack_my_bitch_up.py
+++ python/smack_my_bitch_up.py
@@ -17,7 +17,7 @@ TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN  = os.environ.get('TWILIO_AUTH_TOKEN')
 
 # Phone numbers
-my_number      = '+xxx'
+my_number  = '+xxx'
 her_number = '+xxx'
 
 reasons = [
