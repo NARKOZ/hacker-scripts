895b9228d3d2f7bf7ec7b950e8d03f446bbbf043
Amir Karimi <a.karimi.k@gmail.com>
2015-12-07 14:54:43 +0330

    Run script from bash

--- scala/fucking-coffee.scala
+++ scala/fucking-coffee.scala
@@ -1,17 +1,18 @@
 /*******************************************
  *
- * Get and run Ammonite (http://lihaoyi.github.io/Ammonite/#Ammonite-Shell): 
+ * Get Ammonite (http://lihaoyi.github.io/Ammonite/#Ammonite-Shell): 
  *  $ mkdir ~/.ammonite; curl -L -o ~/.ammonite/predef.scala http://git.io/vR04f
- *  $ curl -L -o amm http://git.io/vR08A; chmod +x amm; ./amm
+ *  $ curl -L -o amm http://git.io/vR08A; chmod +x amm
  *
- * Run script:
- *  @ load.exec("fucking-coffee.scala")
+ * Run script
+ *  $ ./amm fucking-coffee.scala
  *
  *******************************************/
 
 import java.net._
 import java.io._
 import ammonite.ops._
+import ammonite.ops.ImplicitWd._
 
 val coffeeMachineIP = "10.10.42.42"
 val password = "1234"
@@ -25,6 +26,7 @@ if ((%%who "-q").out.string.contains(sys.props("user.name"))) {
   val out = new PrintWriter(telnet.getOutputStream, true)
   val in = new BufferedReader(new InputStreamReader(telnet.getInputStream))
 
+  println(s"Wait for $delayBeforeBrew seconds")
   Thread.sleep(delayBeforeBrew * 1000);
 
   if(in.readLine == passwordPrompt){
