ac58a6f8890ccdb209054b56f2268caa5171e602
syedautherabbas <syedautherabbas@gmail.com>
2016-01-24 20:12:28 +0530

    Signed-off-by: syedautherabbas <syedautherabbas@gmail.com>

--- java/Kumar_Asshole.java
+++ java/Kumar_Asshole.java
@@ -2,29 +2,29 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.*;
+import java.util.regex.*;
 
 import javax.mail.*;
 import javax.mail.internet.*;
 import javax.mail.search.FlagTerm;
 
-/* Before running:
-       U need  to  add Javax Mail JAR. 
-*/
-
 public class Kumar_Asshole {
 
 	public static void main(String[] args) {
-		Kumar_Asshole asshole = new Kumar_Asshole();
-		gmail.read_and_reply();
+		Kumar_Asshole gmail = new Kumar_Asshole();
+		gmail.read();
 	}
 
-	public void read_and_reply() {
+	public void read() {
 		Properties props = new Properties();
-//change the below properties to configure your own.
-		String host = "smtp.gmail.com";
-		String username = "yourmailaddress@example.com goes here";
-		String password = "your password goes here ";
-		String Kumar_mail = "the mail address to be replied to !";
+
+		
+	
+	String host = "smtp.gmail.com";
+	String username = "yourmailaddress@example.com goes here";
+	String password = "your password goes here ";
+	String Kumar_mail = "the mail address to be replied to !";
+		
 		try {
 
 			Session session = Session.getDefaultInstance(props, null);
@@ -42,9 +42,7 @@ public class Kumar_Asshole {
 				if (messages[i].getFrom()[0].toString().contains(Kumar_mail)) {
 
 					String bodytext = null;
-
 					Object content = messages[i].getContent();
-
 					if (content instanceof String) {
 						bodytext = (String) content;
 
@@ -59,7 +57,7 @@ public class Kumar_Asshole {
 
 					Pattern pattern = Pattern.compile("sorry|help|wrong", Pattern.CASE_INSENSITIVE);
 					Matcher matcher = pattern.matcher(bodytext);
-					
+					// check all occurance
 
 					if (matcher.find()) {
 
@@ -67,78 +65,30 @@ public class Kumar_Asshole {
 						Address[] tomail;
 
 						MimeMessage msg = new MimeMessage(session);
-
-						try {
-							msg.setFrom(new InternetAddress(username));
-						} catch (MessagingException e1) {
-							
-							e1.printStackTrace();
-						}
-
-						try {
-							tomail = messages[i].getFrom();
-							String t1 = tomail[0].toString();
-
-							msg.addRecipient(Message.RecipientType.TO, new InternetAddress(t1));
-						} catch (AddressException e1) {
-							
-							e1.printStackTrace();
-						} catch (MessagingException e1) {
-						
-							e1.printStackTrace();
-						}
-
-						
-						try {
-							msg.setSubject("Database fixes");
-						} catch (MessagingException e1) {
-							
-							e1.printStackTrace();
-						}
-
-						
-						try {
-							msg.setText("No problem. I've fixed it. \n\n Please be careful next time.");
-
-						} catch (MessagingException e1) {
-							
-							e1.printStackTrace();
-						}
-
+						msg.setFrom(new InternetAddress(username));
+						tomail = messages[i].getFrom();
+						String t1 = tomail[0].toString();
+						msg.addRecipient(Message.RecipientType.TO, new InternetAddress(t1));
+						msg.setSubject("Database fixes");
+						msg.setText("No problem. I've fixed it. \n\n Please be careful next time.");
 						Transport t = null;
-						try {
-
-							t = session.getTransport("smtps");
-						} catch (NoSuchProviderException e) {
-							
-							e.printStackTrace();
-						}
-						try {
-							try {
-								t.connect(host, username, password);
-							} catch (MessagingException e) {
-								
-								e.printStackTrace();
-							}
-
-							t.sendMessage(msg, msg.getAllRecipients());
-						} catch (MessagingException e) {
-
-							e.printStackTrace();
-
-						} finally {
-						}
-					}
-
-				}
-
+					t = session.getTransport("smtps");
+					t.connect(host, username, password);
+					t.sendMessage(msg, msg.getAllRecipients());
+						
+						
 			}
+		
+
+		} 
+	}
 			inbox.close(true);
 			store.close();
 
-		} catch (Exception e) {
+		}catch(Exception e)
+		{
+			
 			e.printStackTrace();
 		}
 	}
-
 }
