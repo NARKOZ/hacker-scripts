184d527e71ec29c97e2acefed615ef8d99f00a62
Vladimir Riznic <vladimir@filiwiese.com>
2016-11-06 19:12:15 +0100

    refactored to support single config file

--- python/hangover.py
+++ python/hangover.py
@@ -6,31 +6,25 @@ from twilio.rest import TwilioRestClient
 from time import strftime
 import subprocess
 
+import config
+
 # exit if sessions with my username are found
 output = subprocess.check_output('who')
-if 'my_username' in output:
+if config.SYSTEM_USERNAME in output:
     sys.exit()
 
-# returns 'None' if the key doesn't exist
-TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
-TWILIO_AUTH_TOKEN  = os.environ.get('TWILIO_AUTH_TOKEN')
-
-# Phone numbers
-my_number      = '+xxx'
-number_of_boss = '+xxx'
-
 excuses = [
-  'Locked out',
-  'Pipes broke',
-  'Food poisoning',
-  'Not feeling well'
+    'Locked out',
+    'Pipes broke',
+    'Food poisoning',
+    'Not feeling well'
 ]
 
-client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+client = TwilioRestClient(config.TWILIO_ACCOUNT_SID, config.TWILIO_AUTH_TOKEN)
 
 client.messages.create(
-    to=number_of_boss,
-    from_=my_number,
+    to=config.PHONEBOOK['BOSS'],
+    from_=config.PHONEBOOK['ME'],
     body="Gonna work from home. " + random.choice(excuses)
 )
 
