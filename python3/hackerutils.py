394dfb2c76ddc0e81bf70130909948902945e3ab
Ben <wolfnun011@gmail.com>
2020-10-31 10:29:53 +0900

    deleted: 'get_dotenv()'

--- python3/hackerutils.py
+++ python3/hackerutils.py
@@ -4,12 +4,6 @@
 import pathlib
 import subprocess
 
-from dotenv import Dotenv
-
-
-def get_dotenv(filename='.env'):
-    return Dotenv(str(pathlib.Path(__file__).parent / filename))
-
 
 def sh(*args):
     proc = subprocess.Popen(args, stdout=subprocess.PIPE)
@@ -18,6 +12,6 @@ def sh(*args):
 
 
 def get_log_path(name):
-    path = pathlib.Path(__file__).parent / 'logs' / name
+    path = pathlib.Path(__file__).parent / "logs" / name
     path.parent.mkdir(parents=True, exist_ok=True)
     return path
